c9fc79e6e6e8bf4817984a0507681592d113e2aa
Rasel Mahmud <rasel.mahmud.dev@gmail.com>
2023-07-12 01:25:58 +0600

    added monorepo using lerna

--- carts/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const cart = require("./cart");
-const auth = require("../middleware/auth");
-
-
-router.use("/api/carts", auth, cart)
-
-
-module.exports = router
--- users/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const user = require("./user");
-
-
-router.use("/api/users", user)
-
-
-module.exports = router
